0b1878a0265c406e505dac0574f730390d6e6028
Sumit Roy <sumitroy@Sumits-Air.lan>
2025-01-21 00:07:01 +0530

    save q&a to db

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -10,6 +10,8 @@ import { askQuestion } from './actions';
 import { readStreamableValue } from 'ai/rsc';
 import MDEditor from '@uiw/react-md-editor'
 import CodeReferences from './code-references';
+import { api } from '@/trpc/react';
+import { toast } from 'sonner';
 
 
 const AskQuestionCard = () => {
@@ -20,6 +22,9 @@ const AskQuestionCard = () => {
     const [loading, setLoading] = React.useState(false);
     const [fileReferences, setFileReferences] = React.useState<{fileName:string, sourceCode:string, summary:string}[]>()
     const [answer, setAnswer] = React.useState('')
+    const [_q, setQ] = React.useState('');
+    const saveAnswer = api.project.saveAnswer.useMutation();
+
     const onSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
         setQuestion('')
         setAnswer('')
@@ -42,12 +47,32 @@ const AskQuestionCard = () => {
 
   return (
     <>
-    <Dialog open = {open} onOpenChange={setOpen}>
+    <Dialog open = {open} onOpenChange={setOpen}  >
         <DialogContent className='sm:max-w-[80vw]'>
             <DialogHeader>
-                <DialogTitle>
-                    <Image src='/logo.svg' alt='logo' width={40} height={40} />
-                </DialogTitle>
+                <div className="flex items-center gap-2">
+                    <DialogTitle>
+                        <Image src='/logo.svg' alt='logo' width={40} height={40} />
+                    </DialogTitle>
+                    <Button variant='outline' disabled={saveAnswer.isPending} onClick={() => {
+                        saveAnswer.mutate({
+                            projectId: project!.id,
+                            question : _q,
+                            answer,
+                            fileReferences
+                        }, {
+                            onSuccess : () => {
+                                toast.success('Answer saved!')
+                            },
+                            
+                            onError : () => {
+                                toast.error('Failed to save the answer!')
+                            }
+                        })
+                    }}>
+                        Save Answer
+                    </Button>
+                </div>
             </DialogHeader>
 
             <MDEditor.Markdown source={answer} className='max-w-[70vw] !h-full max-h-[40vh] overflow-scroll' />
@@ -65,7 +90,11 @@ const AskQuestionCard = () => {
                     <form onSubmit={onSubmit}>
                         <Textarea placeholder='Which file should i edit to see the homepage?'
                          value={question}
-                         onChange={e => setQuestion(e.target.value)} />
+                         onChange={e => {
+                            setQuestion(e.target.value)
+                            setQ(e.target.value)
+                         }
+                        } />
                         <div className="h-4"></div>
                         <Button type='submit' disabled={loading}>
                             Ask Dionysus!
